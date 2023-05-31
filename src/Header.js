6a4bfe0babc41bbfb95fc822ad9b5787502e7e79
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-05-31 23:08:34 +0500

    setting logo of page

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import logo1 from './images/logo1.png'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <img src={logo1} alt={logo1} width={350}></img>
   )
 }
