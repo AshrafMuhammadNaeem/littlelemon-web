466958520eb26717ceed0513f89181b257b4195e
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-01 15:15:00 +0500

    creating Nav components

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,6 @@ import logo1 from './images/logo1.png'
 
 export const Header = () => {
   return (
-    <img src={logo1} alt={logo1} width={350}></img>
+    <img src={logo1} alt="Logo" width={350}></img>
   )
 }
