0065ac316cc8192a1f4f54257abd738e9e7a9267
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-02 16:33:05 +0500

    Update Footer.js
    adding the necessary HTML elements to the footer component.

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,21 @@ import React from "react";
 
 export const Footer = () => {
     return(
-        <footer>This is footer section</footer>
+        <footer>
+            <img src= {require("./images/Asset 18@4x.png")} alt="Logo" height={300} width={200}/>
+            <h4>Doormat <br/> Navigation</h4>
+            <ul>
+            <li><a href='#'> Home</a> </li>
+            <li><a href='#'> About</a> </li>
+            <li><a href='#'> Menu</a> </li>
+            <li><a href=''> Reservations</a> </li>
+            <li><a href=''> Order</a> </li>
+            <li><a href=''> LogIn</a> </li>
+            </ul>
+            <h4>Contact </h4>
+            <p>Address</p>
+            <p>PhoneNumber</p>
+            <p>Email</p>
+        </footer>
     )
 }
