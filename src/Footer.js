eeea4285a9e2e4dabd7f762920ff0c4dc87ad1fb
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-04 16:50:03 +0500

    Update Footer.js
    adjusting footer content according to Figma dessigned UX-UI

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,17 @@ import React from "react";
 export const Footer = () => {
     return(
         <footer>
-            <img src= {require("./images/Asset 18@4x.png")} alt="Logo" height={300} width={200}/>
-            <h4>Doormat <br/> Navigation</h4>
-            <ul>
+            
+            <div class="container grid grid-3">
+        <div>
+        <img src= {require("./images/Asset 18@4x.png")} alt="Logo" height={300} width={200}/>
+            
+        <p>Copyright &copy; 2022</p>
+        </div>
+        <h4>Doormat <br/> Navigation</h4>
+            
+        <nav>
+        <ul>
             <li><a href='#'> Home</a> </li>
             <li><a href='#'> About</a> </li>
             <li><a href='#'> Menu</a> </li>
@@ -13,10 +21,21 @@ export const Footer = () => {
             <li><a href=''> Order</a> </li>
             <li><a href=''> LogIn</a> </li>
             </ul>
-            <h4>Contact </h4>
+    
+        </nav>
+        <div class="social">
+           <a href="#"><i class="fab fa-github fa-2x"></i></a> 
+           <a href="#"><i class="fab fa-facebook fa-2x"></i></a> 
+           <a href="#"><i class="fab fa-twitter fa-2x"></i></a> 
+           <a href="#"><i class="fab fa-instagram fa-2x"></i></a> 
+        </div>
+        <h4>Contact </h4>
             <p>Address</p>
             <p>PhoneNumber</p>
             <p>Email</p>
+    </div>
+            
+
         </footer>
     )
 }
