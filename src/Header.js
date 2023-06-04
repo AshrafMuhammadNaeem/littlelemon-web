43dc2bb0bae100ceeb67ce2e4afcc915ddd75335
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-04 15:42:15 +0500

    Update Header.js
    adding nav bar in header component
    according to UX UI

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,19 @@ import logo1 from './images/logo1.png'
 
 export const Header = () => {
   return (
-    <header>
+    <header className="container flex">
       <img src={logo1} alt="Logo" width={350}></img>
+      <nav>
+      <ul>
+        <li><a href='#'> Home</a> </li>
+        <li><a href='#'> About</a> </li>
+        <li><a href='#'> Menu</a> </li>
+        <li><a href=''> Reservations</a> </li>
+        <li><a href=''> Order</a> </li>
+        <li><a href=''> LogIn</a> </li>
+    </ul>
+
+    </nav>
     </header>
   )
 }
