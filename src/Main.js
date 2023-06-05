b6beada08075618fd5d6dada9e183fe3eb89dd73
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-05 14:32:09 +0500

    creating Intro section
    first section of the webpage in the main component

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,22 @@ import React from "react";
 export const Main = () => {
     return(
         <main>
-            <h1>This is main section </h1>
+            <section className="Intro bg-primary my-2 py-2">
+                        <div className="restaurant-section">
+                <div className="content">
+                    <div className="text">
+                    <h1>Little Lemon</h1>
+                    <h2>Frankfurt</h2>
+                    <p>Subcontinent's food is known for its rich flavors, aromatic spices, and diverse range of dishes. At our restaurant, we proudly serve authentic Indian cuisine that will tantalize your taste buds. Experience the true essence of India through our carefully crafted menu, featuring traditional favorites and innovative creations. Whether you're craving spicy curries, savory biryanis, or delectable desserts, we have something to satisfy every palate. Reserve a table today and embark on a culinary journey through India.</p>
+                    <button className="reserve-button">Reserve a Table</button>
+                    </div>
+                    <div className="image">
+                    <img src="./images/restaurant-food" alt="Indian Food" />
+                    </div>
+                </div>
+                </div>
+
+            </section>
             <section>
                 This weeks Specials
             </section>
