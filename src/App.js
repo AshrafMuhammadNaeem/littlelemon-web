c9d320e385e07912b3b04e87886a5585389a37fc
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-10 14:49:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Main } from './Main';
-import { Nav } from './Nav';
+
 
 function App() {
   return (
