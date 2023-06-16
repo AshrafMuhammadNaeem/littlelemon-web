1db4eb6427b9daf33398c870afae77cd199afe68
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-16 15:46:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BookingPage } from './BookingPage';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { Main } from './Main';
-import { Router,Routes, Route } from 'react-router-dom';
+// import { Router,Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 function App() {
@@ -14,16 +15,18 @@ function App() {
 
         <Router>
               <Header />
-              <Routes>
+              
+              
                 <Route path="/" element={<Main />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/menu" element={<About />} />
                 <Route path="/reservation" element={<BookingPage />} />
                 <Route path="/order" element={<About />} />
                 <Route path="/logIn" element={<About />} />
-              </Routes>
+              
+              
               <Footer />
-          </Router>
+         </Router>
 
     </>
   );
