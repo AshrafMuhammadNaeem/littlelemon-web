470e2b6bb764010fd3c9899ab08540ec2c776e4c
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-17 14:16:39 +0500

    Update BookingForm.js
    replacing for attribute with react htmlFor

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -4,9 +4,9 @@ export  const  BookingForm = () => {
     return(
       <div className="booking-form">
             <form style="display: grid; max-width: 200px; gap: 20px">
-                    <label for="res-date">Choose date</label>
+                    <label htmlFor="res-date">Choose date</label>
                     <input type="date" id="res-date"/>
-                    <label for="res-time">Choose time</label>
+                    <label htmlFor="res-time">Choose time</label>
                     <select id="res-time ">
                         <option>17:00</option>
                         <option>18:00</option>
@@ -15,9 +15,9 @@ export  const  BookingForm = () => {
                         <option>21:00</option>
                         <option>22:00</option>
                     </select>
-                    <label for="guests">Number of guests</label>
+                    <label htmlFor="guests">Number of guests</label>
                     <input type="number" placeholder="1" min="1" max="10" id="guests"/>
-                    <label for="occasion">Occasion</label>
+                    <label htmlFor="occasion">Occasion</label>
                     <select id="occasion">
                         <option>Birthday</option>
                         <option>Anniversary</option>
