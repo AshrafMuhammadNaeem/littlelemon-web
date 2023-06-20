98362e29c497c99565b1ba826142fc1597b7dcd7
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-20 23:20:12 +0500

    Update BookingPage.js

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,21 +1,12 @@
-import React, {useState, useReducer} from 'react'
+import React, {useState} from 'react'
 import { BookingForm } from './BookingForm'
 
 
-function reducer(selectedDate) {
-    
-  return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
-}
-
-
 export const BookingPage = () => {
-
-  const initialState = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
-  const [time, dispatch] = useReducer(reducer, initialState)
-
+  const [time, setTime] = useState("17:00")
   return (
     <>
-      <BookingForm time={time} dispatch={dispatch}/>
+      <BookingForm time={time} setTime={setTime}/>
     </>
   )
 }
