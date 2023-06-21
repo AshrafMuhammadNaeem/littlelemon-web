b75eab0dfbddba759770b991091bfd5fb373d4ac
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-21 16:30:12 +0500

    Update BookingPage.js
    replacing useState with useReducer

--- src/BookingPage.js
+++ src/BookingPage.js
@@ -1,12 +1,25 @@
 import React, {useState} from 'react'
 import { BookingForm } from './BookingForm'
 
+function updateTimes(state, selectedDate) {
+  // Logic to update availableTimes based on the selectedDate
+  // For now, return the same available times
+  return ['17:00 pm', '18:00 pm', '19:00', '20:00', '21:00', '22:00'];
+}
+
+function initializeTimes() {
+  // Logic to initialize availableTimes
+  return ['17:00 pm', '18:00 pm', '19:00', '20:00', '21:00', '22:00'];
+}
+
+
+
 
 export const BookingPage = () => {
-  const [time, setTime] = useState("17:00")
+  const [time, dispatch] = useReducer(updateTimes, null, initializeTimes);
   return (
     <>
-      <BookingForm time={time} setTime={setTime}/>
+      <BookingForm time={time} setTime={dispatch} />
     </>
   )
 }
