79bb7102cb3f834f68df08eae9575fab4513a89b
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-06-24 15:12:51 +0500

    Update App.test.js
    unit test for reducer function

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import { BookingForm } from './BookingForm';
+import { initializeTimes, updateTimes } from "./reducerFunctions";
 
 test('renders learn react link', () => {
   render(<App />);
@@ -15,3 +16,24 @@ test("find the BookingForm heading", () =>{
   const headingElement = screen.getByText("Booking Form");
   expect(headingElement).toBeInTheDocument();
 });
+
+
+// Unit test for initializeTimes
+test("initializeTimes returns the expected initial state", () => {
+  const expectedInitialState = [
+    '17:00 pm', '18:00 pm', '19:00', '20:00', '21:00', '22:00'
+  ];
+  const initialState = initializeTimes();
+  expect(initialState).toEqual(expectedInitialState);
+});
+
+// Unit test for updateTimes
+test("updateTimes returns the same state value", () => {
+  const state = [
+    '17:00 pm', '18:00 pm', '19:00', '20:00', '21:00', '22:00'
+  ];
+  const selectedDate = "2023-06-18"; // example selected date
+  const updatedState = updateTimes(state, selectedDate);
+  expect(updatedState).toEqual(state);
+});
+
