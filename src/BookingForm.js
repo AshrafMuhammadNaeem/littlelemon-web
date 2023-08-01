3b4c7585c52cbb1fcb2768271790e6d071e98f3d
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-08-01 13:51:38 +0500

    Update BookingForm.js

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -77,84 +77,84 @@ export  const  BookingForm = ({
         
 
     return(
-      <div className="">
-        <div className="booking-form">
-          <h2> Booking Form </h2>
-            <form onSubmit={handleSubmit}  >
-              <div className="form">
-              <div className="icon-wraper">
-                <label htmlFor="res-date"> Date</label>
-                <DatePicker  className={isDatePickerOpen ? "res-date-inactive " : "res-date-active"} name= "date"
-                  placeholderText="Select Date"
-                  selected={isDateSelected ? formData.date : null} 
-                  onChange={(date) => { handleDateChange(date); handleDateSelection(date); }}
-                  onFocus={handleDatePickerOpen}
-                  onBlur={handleDatePickerClose}
-                  min={new Date().toISOString().split("T")[0]}
-                  />
-                <FontAwesomeIcon icon={ isDatePickerOpen ? faChevronUp : faChevronDown} className={isDatePickerOpen ? "icon-2-inactive " : "icon-2-active"} />
-                <FontAwesomeIcon icon={faCalendar} fontSize={30} className={isDatePickerOpen ? "icon-inactive " : "icon-active"} />
-              </div>
-              <div className="icon-wraper">
-                <label htmlFor="res-time">Choose time</label>
-                      <FontAwesomeIcon icon={faClock} fontSize={20} /><select >
-                                          {availableTimes.availableTimes.map((time, idx) => (
-                                            <option key={idx}
-                                            value={time}
-                                            onChange={(e) => {
-                                              handleTimeSelection(e, time);
-                                              handleChange(e);
-                                            }}
-                                            disabled={availableTimes.selectedTime === time}>
-                                              {time}</option>
+      <div className="form-container">
+          <div className="booking-form">
+                <h2> Booking Form </h2>
+              <form onSubmit={handleSubmit}  >
+                    <div className="form">
+                    <div className="icon-wraper">
+                      <label htmlFor="res-date"> Date</label>
+                      <DatePicker  className={isDatePickerOpen ? "res-date-inactive " : "res-date-active"} name= "date"
+                        placeholderText="Select Date"
+                        selected={isDateSelected ? formData.date : null} 
+                        onChange={(date) => { handleDateChange(date); handleDateSelection(date); }}
+                        onFocus={handleDatePickerOpen}
+                        onBlur={handleDatePickerClose}
+                        min={new Date().toISOString().split("T")[0]}
+                        />
+                      <FontAwesomeIcon icon={ isDatePickerOpen ? faChevronUp : faChevronDown} className={isDatePickerOpen ? "icon-2-inactive " : "icon-2-active"} />
+                      <FontAwesomeIcon icon={faCalendar} fontSize={30} className={isDatePickerOpen ? "icon-inactive " : "icon-active"} />
+                    </div>
+                    <div className="icon-wraper">
+                      <label htmlFor="res-time">Choose time</label>
+                            <FontAwesomeIcon icon={faClock} fontSize={20} className="time-icon" /><select className="time-input">
+                                                {availableTimes.availableTimes.map((time, idx) => (
+                                                  <option key={idx}
+                                                  value={time}
+                                                  onChange={(e) => {
+                                                    handleTimeSelection(e, time);
+                                                    handleChange(e);
+                                                  }}
+                                                  disabled={availableTimes.selectedTime === time}>
+                                                    {time}</option>
 
-                                          ))}
-                      </select>
-              </div>
-              <div className="icon-wraper">
-                <label htmlFor="guests">Number of guests</label>
-                <FontAwesomeIcon icon={faUser} fontSize={20}  />
-                <input type="number" placeholder="1" min="1" max="10" id="guests"
-                name="guests" value={formData.guests} 
-                onChange={handleChange}
-                />
-                   
-              </div>
-              <div>
-              <label htmlFor="occasion">Occasion</label>
-              
-              <FontAwesomeIcon icon={faChampagneGlasses} fontSize={20} /><select id="occasion" name="occasion" value={formData.occasion} onChange={handleChange}>
-                        <option value="Birthday">Birthday</option>
-                        <option value="Anniversary">Anniversary</option>
-                        <option value="Work Promotion">Work Promotion</option>
-                        <option value="Engagement">Engagement</option>
-                    </select>
-              </div>
+                                                ))}
+                            </select>
+                    </div>
+                    <div className="icon-wraper">
+                      <label htmlFor="guests">Number of guests</label>
+                      <FontAwesomeIcon icon={faUser} fontSize={20} className="guests-icon" />
+                      <input type="number" placeholder="1" min="1" max="10" id="guests" className="guests-input"
+                      name="guests" value={formData.guests} 
+                      onChange={handleChange}
+                      />
+                        
+                    </div>
+                    <div>
+                    <label htmlFor="occasion">Occasion</label>
+                    
+                    <FontAwesomeIcon icon={faChampagneGlasses} fontSize={20} /><select id="occasion" name="occasion" value={formData.occasion} onChange={handleChange}>
+                              <option value="Birthday">Birthday</option>
+                              <option value="Anniversary">Anniversary</option>
+                              <option value="Work Promotion">Work Promotion</option>
+                              <option value="Engagement">Engagement</option>
+                          </select>
+                    </div>
 
-                      <div>
-                      <label htmlFor="indoor">Indoor seating</label>
-                      <input type="radio" id="indoor" name="indoor seating" value= "Indoor Seating"/>
-                      </div>
-                      <div>
-                      <label htmlFor="indoor">Outdoor seating</label>
-                      <input type="radio" id="outdoor" name="outdoor seating" value= "Outdoor Seating"/>
-                      </div>
-                     
+                            <div>
+                            <label htmlFor="indoor">Indoor seating</label>
+                            <input type="radio" id="indoor" name="indoor seating" value= "Indoor Seating"/>
+                            </div>
+                            <div>
+                            <label htmlFor="indoor">Outdoor seating</label>
+                            <input type="radio" id="outdoor" name="outdoor seating" value= "Outdoor Seating"/>
+                            </div>
+                          
 
-              </div>
+                    </div>
 
-            </form>
+              </form>
 
-          </div>
+            </div>
 
-            <div>
-                   <img src={restaurant}  width={200}  alt="restaurant inside"></img>
-                   <img src={restaurant}  width={200}  alt="restaurant inside"></img>
-                   <img src={restaurant}  width={200}  alt="restaurant inside"></img>
-              </div>
-              <div>
-                   <input type="submit" value="Make Your reservation"/>
-              </div>
+                  <div>
+                        <img src={restaurant}  width={200}  alt="restaurant inside"></img>
+                        <img src={restaurant}  width={200}  alt="restaurant inside"></img>
+                        <img src={restaurant}  width={200}  alt="restaurant inside"></img>
+                    </div>
+                    <div>
+                        <input type="submit" value="Make Your reservation"/>
+                    </div>
       </div>
     )
 }
