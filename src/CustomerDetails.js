cbda1b2b87f951df93fb2f0bc6f137d52409c69e
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-08-08 23:01:35 +0500

    Update CustomerDetails.js
    passing props from BokkingPage.js

--- src/CustomerDetails.js
+++ src/CustomerDetails.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
-export const CustomerDetails = (props) => {
+export const CustomerDetails = ({formData, setFormData, submitForm}) => {
   return (
     <div>CustomerDetails
       <form>
         <label htmlFor='firstname'>First Name </label>
-        <input type='text' value={props.firstname} name='firstname' id='firstname' className=''/>
+        <input type='text' value={formData.fname} name='firstname' id='firstname' className=''/>
       </form>
     </div>
   )
