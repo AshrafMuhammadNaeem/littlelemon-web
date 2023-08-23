7415eb76435ca20929c570d1941d1e5087aa3afc
Naeem Ashraf <naeemashraf.tud@gmail.com>
2023-08-23 22:25:45 +0500

    Update CustomerDetails.js

--- src/CustomerDetails.js
+++ src/CustomerDetails.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 
 export const CustomerDetails = ({formData, setFormData, submitForm}) => {
+
+  const handleNameChange = (e) => {
+    e.preventDefault();
+    const {name, value} = e.target;
+    setFormData((data) => ({
+      ...data, 
+      [name] : value,
+    }))
+  }
+
+
   return (
     <div>CustomerDetails
       <form>
         <label htmlFor='firstname'>First Name </label>
-        <input type='text' value={formData.fname} name='firstname' id='firstname' className=''/>
+        <input type='text' value={formData.fname} name='firstname' id='firstname' className='' onChange={handleNameChange}/>
       </form>
     </div>
   )
